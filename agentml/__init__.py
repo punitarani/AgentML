9bf998fe318fa4f1b646404da030faf29fcd9b2b
Punit Arani <punitsai36@gmail.com>
2023-11-22 10:41:52 -0700

    Load the environment variables

--- agentml/__init__.py
+++ agentml/__init__.py
@@ -1,1 +1,9 @@
 """agentml package"""
+
+from dotenv import load_dotenv
+
+from config import PROJECT_PATH
+
+env_loaded = load_dotenv(PROJECT_PATH.joinpath(".env"))
+if not env_loaded:
+    raise RuntimeError("Failed to load environment variables")
