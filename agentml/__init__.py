a5d7ffa459f0cdfd28a1867225de6dcef1aadaa7
Punit Arani <punitsai36@gmail.com>
2023-11-22 12:46:29 -0700

    Revert "Add PYTHON_EXE env var"
    This reverts commit b4d7d78509b2840c17b5ddb0c9d129ba7a54e05a.

--- agentml/__init__.py
+++ agentml/__init__.py
@@ -1,7 +1,5 @@
 """agentml package"""
 
-import os
-
 from dotenv import load_dotenv
 
 from config import PROJECT_PATH
@@ -13,6 +11,3 @@ __all__ = ["Manager"]
 env_loaded = load_dotenv(PROJECT_PATH.joinpath(".env"))
 if not env_loaded:
     raise RuntimeError("Failed to load environment variables")
-
-if not os.getenv("PYTHON_EXE"):
-    raise RuntimeError("PYTHON_EXE environment variable not set")
