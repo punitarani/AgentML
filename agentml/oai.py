5de8eeefe23a3e6f8d3d16b29875de63c495bd27
Punit Arani <punitsai36@gmail.com>
2023-11-23 13:50:16 -0700

    Load env in oai.py

--- agentml/oai.py
+++ agentml/oai.py
@@ -2,7 +2,14 @@
 
 import os
 
+from dotenv import load_dotenv
 from openai import OpenAI
+
+from config import PROJECT_PATH
+
+env_loaded = load_dotenv(PROJECT_PATH.joinpath(".env"))
+if not env_loaded:
+    raise RuntimeError("Failed to load environment variables")
 
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 assert OPENAI_API_KEY is not None, "OPENAI_API_KEY environment variable not set"
