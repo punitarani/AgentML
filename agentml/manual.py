8f000bd31d3fe97eb1329115ef0f5c09b0385e8d
Punit Arani <punitsai36@gmail.com>
2023-11-22 18:13:42 -0700

    Add manual Manager

---
+++ agentml/manual.py
@@ -0,0 +1,152 @@
+"""agentml/manual.py"""
+
+from pathlib import Path
+from typing import Type
+from uuid import UUID
+
+from agentml.models import LlmMessage, LlmRole
+
+from .agents import Agent, Coder, Planner, Vision
+from .sandbox import Sandbox
+
+
+class Manager:
+    """Manual Manager"""
+
+    def __init__(
+        self,
+        goal: str,
+        csv: Path,
+        session_id: UUID,
+    ) -> None:
+        """
+        Agent constructor
+
+        Args:
+            goal (str): goal of the agent
+            csv (Path): CSV file path of the dataset
+            session_id (UUID): Session ID
+        """
+
+        # Ensure the CSV file exists
+        if not csv.exists():
+            raise FileNotFoundError(f"Agent: CSV file not found: {csv}")
+
+        self.goal = goal
+        self.csv = csv
+        self.session_id = session_id
+
+        self.sandbox = Sandbox.create(session_id=session_id, files=[csv])
+
+        # Chat history
+        self.messages: list[LlmMessage] = [
+            LlmMessage(
+                role=LlmRole.SYSTEM,
+                content="Overarching Goal: " + self.goal,
+            ),
+        ]
+
+        # Queue of tasks to run
+        self.tasks: list[dict[callable, str]] = [
+            {
+                Planner: "Outline the steps to learn about the dataset and its features to achieve the goal"
+            }
+        ]
+
+        # Stored agent instances
+        self.agents = {}
+
+    def run(self) -> list[LlmMessage]:
+        """Run the agent"""
+
+        if not self.tasks:
+            print("Manager.run: No tasks to run.")
+            return []
+
+        task = self.tasks[0]
+
+        for agent_class, objective in task.items():
+            print(
+                f"Manager.run: Running agent {agent_class.__name__} with objective: {objective}"
+            )
+
+            # Instantiate the agent
+            agent_instance = agent_class(
+                session_id=self.session_id,
+                objective=objective,
+                messages=[*self.messages],
+            )
+
+            # Store the agent instance
+            self.agents[agent_class.__name__] = agent_instance
+
+            return agent_instance.run()
+
+    def validate_run(self, messages: list[LlmMessage]) -> None:
+        """User validate the run"""
+
+        if not self.tasks:
+            print("Manager.validate_run: No tasks to validate.")
+            return
+
+        # Pop the task
+        task = self.tasks.pop(0)
+        print(f"Manager.validate_run: Popped task: {task}")
+
+        # Add the messages to the chat history
+        self.messages.extend(messages)
+
+        # Handle different agents
+        for agent_class, objective in task.items():
+            agent_name = agent_class.__name__
+            print(f"Manager.validate_run: Handling agent: {agent_name}")
+
+            # Check if the agent instance exists
+            if agent_name in self.agents:
+                agent_instance = self.agents[agent_name]
+
+                if agent_class == Planner:
+                    # Add the tasks to the queue
+                    print(
+                        f"Manager.validate_run: Adding tasks to queue: {agent_instance.plan}"
+                    )
+                    self.tasks.extend(
+                        [
+                            {self.get_agent(task["tool"]): task["objective"]}
+                            for task in agent_instance.plan
+                        ]
+                    )
+
+                del self.agents[agent_name]
+
+            else:
+                print(f"Manager.validate_run: No instance found for {agent_name}")
+
+    def add_task(self, task: dict[str, str]) -> None:
+        """Add a task to the queue"""
+        for agent, objective in task.items():
+            print(f"Manager.add_task: Adding task: {task}")
+            self.tasks.append({self.get_agent(agent): objective})
+
+    def delete_task(self, idx: int) -> None:
+        """Delete a task from the queue"""
+        print(f"Manager.delete_task: Deleting task at index: {idx}")
+        del self.tasks[idx]
+
+    @staticmethod
+    def get_agent(agent: str | Type[Agent]) -> Type[Agent]:
+        """Get the agent"""
+
+        # Check if the agent is already a class of type Agent
+        if isinstance(agent, type) and issubclass(agent, Agent):
+            return agent
+
+        match agent:
+            case "Coder":
+                return Coder
+            case "Planner":
+                return Planner
+            case "Vision":
+                return Vision
+            case _:
+                raise ValueError(f"Manager.get_agent: Invalid agent: {agent}")
